a26623b80745a3d8c49d62ad4176300dc9f1f294
Kacper Zys <kacperzys13@gmail.com>
2025-10-03 08:40:11 +0200

    Create basic project architecture

---
+++ Flashcards.KacperZys/Flashcards.KacperZys/View/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿namespace Flashcards.KacperZys.View;
+internal class MainMenu
+{
+}
+
+public enum Option
+{
+    Manage_Stacks,
+    Manage_Flashcards,
+    Study,
+    View_Study_Session_Data,
+    Exit
+}
